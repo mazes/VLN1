2c3ba2721aeadc5e82267510ea7d9959a69b3130
mazes <eirikurb14@ru.is>
2014-11-27 14:17:44 +0000

    updated

--- date.cpp
+++ date.cpp
@@ -3,3 +3,15 @@
 date::date()
 {
 }
+
+date::date(int d, int m, int y)
+{
+    day = d;
+    month = m;
+    year = y;
+}
+
+void date:: get_date()
+{
+    std::cout << d << "." << m << "." << y << std::endl;
+}
